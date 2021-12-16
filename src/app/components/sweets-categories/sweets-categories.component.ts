92c7f86c7f2f6e136414cdb62f253bb806179887
Martin <martin@createdigitalsolutions.com>
2021-12-16 16:39:58 +0000

    top stuff

--- src/app/components/sweets-categories/sweets-categories.component.ts
+++ src/app/components/sweets-categories/sweets-categories.component.ts
@@ -1,49 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sweets-categories',
-  templateUrl: './sweets-categories.component.html',
-  styleUrls: ['./sweets-categories.component.css']
+  selector: "app-sweets-categories",
+  templateUrl: "./sweets-categories.component.html",
+  styleUrls: ["./sweets-categories.component.scss"],
 })
 export class SweetsCategoriesComponent implements OnInit {
-
   categoryList: any = [];
 
   constructor() {
-    this.categoryList = [{
-      categoryTitle: "Test",
-      categoryDescription: "Description",
-      imageUrl: "assets/images/30day.png",
-      target: "category/#" 
-    },{
-      categoryTitle: "Test",
-      categoryDescription: "Description",
-      imageUrl: "assets/images/30day.png",
-      target: "category/#" 
-    },{
-      categoryTitle: "Test",
-      categoryDescription: "Description",
-      imageUrl: "assets/images/30day.png",
-      target: "category/#" 
-    },{
-      categoryTitle: "Test",
-      categoryDescription: "Description",
-      imageUrl: "assets/images/30day.png",
-      target: "category/#" 
-    },{
-      categoryTitle: "Test",
-      categoryDescription: "Description",
-      imageUrl: "assets/images/30day.png",
-      target: "category/#" 
-    },{
-      categoryTitle: "Test",
-      categoryDescription: "Description",
-      imageUrl: "assets/images/30day.png",
-      target: "category/#" 
-    }]
+    this.categoryList = [
+      {
+        categoryTitle: "Test",
+        categoryDescription: "Description",
+        imageUrl: "assets/images/30day.png",
+        target: "category/#",
+      },
+      {
+        categoryTitle: "Test",
+        categoryDescription: "Description",
+        imageUrl: "assets/images/30day.png",
+        target: "category/#",
+      },
+      {
+        categoryTitle: "Test",
+        categoryDescription: "Description",
+        imageUrl: "assets/images/30day.png",
+        target: "category/#",
+      },
+      {
+        categoryTitle: "Test",
+        categoryDescription: "Description",
+        imageUrl: "assets/images/30day.png",
+        target: "category/#",
+      },
+    ];
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
