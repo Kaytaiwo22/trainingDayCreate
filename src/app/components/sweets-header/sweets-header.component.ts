dc06263ec537f93aa1d6e070816bab3899145eba
Yasin Abdullah Mohammad <yasin@createdigitalsolutions.com>
2021-12-16 16:33:16 +0000

    Logo and Search bar

--- src/app/components/sweets-header/sweets-header.component.ts
+++ src/app/components/sweets-header/sweets-header.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-sweets-header',
-  templateUrl: './sweets-header.component.html',
-  styleUrls: ['./sweets-header.component.css']
+  selector: "app-sweets-header",
+  templateUrl: "./sweets-header.component.html",
+  styleUrls: ["./sweets-header.component.scss"],
 })
 export class SweetsHeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
