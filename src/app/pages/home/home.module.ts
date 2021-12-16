69a9d590a005ce4935afddfc2cb57b09f85cf650
Kay Taiwo <66003680+Kaytaiwo22@users.noreply.github.com>
2021-12-16 12:37:02 +0000

    Home Hero Added - Images added

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home.component";
+import {HeroModule} from "../../components/hero/hero.module";
 
 const routes: Routes = [
   {
@@ -21,7 +22,7 @@ export class ConfirmationRoutingModule {}
  * The Module
  */
 @NgModule({
-  imports: [CommonModule, RouterModule, ConfirmationRoutingModule],
+    imports: [CommonModule, RouterModule, ConfirmationRoutingModule, HeroModule],
   declarations: [HomeComponent],
   providers: []
 })
