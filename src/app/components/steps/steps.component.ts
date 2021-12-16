600a96dde8a835d53b172e6fee5294269a652c74
Kay Taiwo <66003680+Kaytaiwo22@users.noreply.github.com>
2021-12-16 15:43:25 +0000

    steps UI Design

--- src/app/components/steps/steps.component.ts
+++ src/app/components/steps/steps.component.ts
@@ -7,21 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StepsComponent implements OnInit {
 
-  public steps: {
-    title: string;
-    description: string;
-  }[]
+  public currentStep: boolean;
+  counter = 0;
+  public steps= [
+      {title: "Select Bag Size", description: "We say the bigger the better!"},
+    {title: "Fill It Up", description: "Select up to 10 items for your bag"},
+    {title: "Fill in delivery details", description: "It will be on its way soon!"},]
 
   constructor() { }
 
   ngOnInit(): void {
-    this.setupSteps();
+
   }
 
-  setupSteps() {
-    this.steps.push(
-      {title: "Select Bag Size", description: "We say the bigger the better!"},
-      {title: "Fill It Up", description: "Select up to 10 items for your bag"},
-    );
+  nextSteps() {
+    this.counter++;
   }
+
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from "@angular/common";
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home.component";
 import {HeroModule} from "../../components/hero/hero.module";
+import {StepsModule} from "../../components/steps/steps.module";
 
 const routes: Routes = [
   {
@@ -22,7 +23,7 @@ export class ConfirmationRoutingModule {}
  * The Module
  */
 @NgModule({
-    imports: [CommonModule, RouterModule, ConfirmationRoutingModule, HeroModule],
+  imports: [CommonModule, RouterModule, ConfirmationRoutingModule, HeroModule, StepsModule],
   declarations: [HomeComponent],
   providers: []
 })
