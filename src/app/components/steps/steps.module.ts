91e8f5821574afa793c77c85a8544940d4d49e9a
Kay Taiwo <66003680+Kaytaiwo22@users.noreply.github.com>
2021-12-16 16:35:09 +0000

    steps

--- src/app/components/steps/steps.module.ts
+++ src/app/components/steps/steps.module.ts
@@ -5,10 +5,11 @@ import {NgModule} from "@angular/core";
 import { StepsComponent} from "./steps.component";
 import { CommonModule } from '@angular/common';
 import {RouterModule} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, ReactiveFormsModule],
   entryComponents: [StepsComponent],
   declarations: [StepsComponent],
   exports: [StepsComponent]
