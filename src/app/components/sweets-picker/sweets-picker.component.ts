38520bef94a172441f9623f642286cd7bc76dcc5
Yasin Abdullah Mohammad <yasin@createdigitalsolutions.com>
2022-01-12 10:38:29 +0000

    no message

--- src/app/components/sweets-picker/sweets-picker.component.ts
+++ src/app/components/sweets-picker/sweets-picker.component.ts
@@ -10,7 +10,25 @@ export class SweetsPickerComponent {
     { url: "../assets/images/sweets-jar1.png", description: "gummy sweet" },
     { url: "../assets/images/sweets-jar1.png", description: "gummy sweet" },
     { url: "../assets/images/sweets-jar1.png", description: "gummy sweet" },
+    { url: "../assets/images/sweets-jar1.png", description: "gummy sweet" },
+    { url: "../assets/images/sweets-jar1.png", description: "gummy sweet" },
   ];
 
   constructor() {}
+
+  ngOnInit() {}
+  quantity: number = 1;
+  i = 1;
+  plus() {
+    if (this.i != 20) {
+      this.i++;
+      this.quantity = this.i;
+    }
+  }
+  minus() {
+    if (this.i != 1) {
+      this.i--;
+      this.quantity = this.i;
+    }
+  }
 }
